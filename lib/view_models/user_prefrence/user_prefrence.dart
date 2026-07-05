fb0c27f555a3825aab2cf7dcf46ddb9db4219968
Your GitHub aleem219 <Your GitHub qazialeem21@gmail.com>
2026-07-06 00:38:40 +0530

    updated code for login view model

---
+++ lib/view_models/user_prefrence/user_prefrence.dart
@@ -0,0 +1,29 @@
+import 'package:mvvm_login/models/login/user_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class UserPreference {
+  Future<bool> saveUser(UserModel responseModel) async {
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    // Use setString to store the token in SharedPreferences
+    sp.setString('accessToken', responseModel.accessToken.toString());
+    sp.setBool('isLogin', responseModel.isLogin!);
+    return true;
+  }
+
+  Future<UserModel> getUser() async {
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    String? token = sp.getString('token');
+    bool? isLoign = sp.getBool('isLogin');
+    return UserModel(
+      accessToken: token,
+      isLogin: isLoign,
+    );
+  }
+
+  //to delete user data
+  Future<bool> removeUser() async {
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    sp.clear();
+    return true;
+  }
+}
