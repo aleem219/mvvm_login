d7b473547afe4b765ae7507340c9c42fcf7c0fe1
Your GitHub aleem219 <Your GitHub qazialeem21@gmail.com>
2026-07-07 17:32:53 +0530

    updated code for update setState for fav button in UserCard

--- lib/views/user/userlist/widgets/user_card.dart
+++ lib/views/user/userlist/widgets/user_card.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mvvm_login/models/user/userList/user_list.dart';
 
-class UserCard extends StatelessWidget {
+class UserCard extends StatefulWidget {
   final UserList user;
   final VoidCallback? onTap;
   final VoidCallback? onButtonTap;
@@ -14,10 +14,24 @@ class UserCard extends StatelessWidget {
   });
 
   @override
+  State<UserCard> createState() => _UserCardState();
+}
+
+class _UserCardState extends State<UserCard> {
+  bool isFavorite = false;
+
+  void _handleButtonTap() {
+    setState(() {
+      isFavorite = !isFavorite;
+    });
+    widget.onButtonTap?.call();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return InkWell(
       borderRadius: BorderRadius.circular(20),
-      onTap: onTap,
+      onTap: widget.onTap,
       child: Container(
         margin: const EdgeInsets.symmetric(vertical: 8),
         padding: const EdgeInsets.all(16),
@@ -31,7 +45,7 @@ class UserCard extends StatelessWidget {
             CircleAvatar(
               radius: 32,
               backgroundColor: Colors.white,
-              backgroundImage: NetworkImage(user.image),
+              backgroundImage: NetworkImage(widget.user.image),
             ),
             const SizedBox(width: 16),
             Expanded(
@@ -39,7 +53,7 @@ class UserCard extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    user.fullName,
+                    widget.user.fullName,
                     style: const TextStyle(
                       fontSize: 20,
                       fontWeight: FontWeight.bold,
@@ -48,12 +62,12 @@ class UserCard extends StatelessWidget {
                   ),
                   const SizedBox(height: 4),
                   Text(
-                    user.email,
+                    widget.user.email,
                     style: const TextStyle(fontSize: 15, color: Colors.white),
                   ),
                   const SizedBox(height: 4),
                   Text(
-                    'phone: ${user.phone}',
+                    'phone: ${widget.user.phone}',
                     style: const TextStyle(
                       fontSize: 14,
                       fontStyle: FontStyle.italic,
@@ -65,8 +79,11 @@ class UserCard extends StatelessWidget {
             ),
             // Button lives here, separate from the card's own InkWell
             IconButton(
-              icon: const Icon(Icons.favorite_border, color: Colors.white),
-              onPressed: onButtonTap,
+              icon: Icon(
+                isFavorite ? Icons.favorite : Icons.favorite_border,
+                color: isFavorite ? Colors.red : Colors.white,
+              ),
+              onPressed: _handleButtonTap,
             ),
           ],
         ),
